580134b111ec012590b478243b2e123ceb597613
Asik007 <72676331+Asik007@users.noreply.github.com>
2026-08-19 16:27:39 -0500

    basic blog setup and tinkering with navbar

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,12 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const NAV_LINKS = [
+  { title: 'Home', href: '/' },
+  { title: 'About', href: '/about' },
+  { title: 'Blog', href: '/blog' },
+];
+
+export const LINKED_IN_URL = 'https://www.linkedin.com/in/asik007/';
+export const GITHUB_PROFILE_URL = 'https://github.com/asik007';
