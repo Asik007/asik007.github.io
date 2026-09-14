fba7570e71feaf9a93a34d32741c87298fbe0a96
Asik007 <asikl2@uic.edu>
2026-09-14 12:42:58 -0500

    title change and such

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Advik S.';
+
+export const SITE_DESCRIPTION = 'Welcome to my personal and professional website!';
 
 export const NAV_LINKS = [
   // { title: 'Home', href: '/' },
